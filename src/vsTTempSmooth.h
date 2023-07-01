91836c8936ea61c3341e22ce54f83b4c10dc1347
Asd-g <65298684+Asd-g@users.noreply.github.com>
2023-07-01 13:52:34 +0300

    Removed commented out
    (cherry picked from commit https://github.com/DTL2020/AviSynth-vsTTempSmooth/commit/fca3208b016159c4f608cde324f6558ae742d2fb)

--- src/vsTTempSmooth.h
+++ src/vsTTempSmooth.h
@@ -58,9 +58,6 @@ class TTempSmooth : public GenericVideoFilter
 
     float (*compare)(PVideoFrame& src, PVideoFrame& src1, const int bits_per_pixel) noexcept;
 
-    //	void filterI_mode2_C_uint8(PVideoFrame src[15], PVideoFrame pf[15], PVideoFrame& dst, const int fromFrame, const int toFrame, const int plane);
-    //	void filterI_mode2_C_uint16(PVideoFrame src[15], PVideoFrame pf[15], PVideoFrame& dst, const int fromFrame, const int toFrame, const int plane);
-
     template<typename T>
     void filterI_mode2_C(PVideoFrame src[MAX_TEMP_RAD], PVideoFrame pf[MAX_TEMP_RAD], PVideoFrame& dst, const int fromFrame, const int toFrame, const int plane);
 
